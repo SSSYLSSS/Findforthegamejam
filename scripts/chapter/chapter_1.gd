0902eae713eecc093075c1de2b818466cf32547c
SSSYLSSS <120182557+SSSYLSSS@users.noreply.github.com>
2025-10-06 18:43:33 +0800

    add_asset

--- scripts/chapter/chapter_1.gd
+++ scripts/chapter/chapter_1.gd
@@ -4,6 +4,8 @@ var force_core_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	SoundManager.play_bgm("Intro2Flat")
+	SoundManager.stop_bgm("Intro1")
 	force_core_node=get_tree().current_scene.get_node("ForceCores")
 	var all_children = force_core_node.get_children()
 	for child in all_children:
